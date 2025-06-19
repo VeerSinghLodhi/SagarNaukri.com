63230500ff4d9901cdcdfacee806cfe1e9ed2a22
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-19 21:21:25 +0530

    Update JobsRepository.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/Jobs/JobsRepository.java
+++ SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/Jobs/JobsRepository.java
@@ -3,4 +3,6 @@ package com.example.SagarNaukri.com.Jobs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobsRepository extends JpaRepository<Jobs , Integer> {
+    List<Jobs> findByCompanyid(int companyid);
+    Optional<Jobs>findByJobid(int id);
 }
