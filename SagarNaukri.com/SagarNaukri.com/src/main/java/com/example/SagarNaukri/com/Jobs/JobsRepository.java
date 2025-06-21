312f9a6d705f8d82ecb9c666eb06d3de1811dc41
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-21 09:08:59 +0530

    Update JobsRepository.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/Jobs/JobsRepository.java
+++ SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/Jobs/JobsRepository.java
@@ -1,13 +1,25 @@
 package com.example.SagarNaukri.com.Jobs;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface JobsRepository extends JpaRepository<Jobs , Integer> {
-   
-    @Query("select e from Jobs e where e.companyid=:companyid order by e.jobid")
-    List<Jobs>findByCompanyid(@Param("companyid")int companyid);
 
+    @Query("select e from Jobs e where e.companyid=:companyid and e.isenabled=true order by e.jobid")
+    List<Jobs>findByCompanyid(@Param("companyid")int companyid);
 
 //    List<Jobs> findByCompanyid(int companyid);
     Optional<Jobs>findByJobid(int id);
+
+    @Modifying
+    @Transactional
+    @Query("update Jobs e set e.isenabled = false where e.jobid =:jobid")
+    void deleteJobById(@Param("jobid") int jobid);
+
 }
