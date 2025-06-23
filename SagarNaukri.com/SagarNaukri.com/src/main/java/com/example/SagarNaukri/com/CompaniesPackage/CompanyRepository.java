d98916b013dabbc4cc9db8363d412b76259e6d11
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-06-24 00:31:40 +0530

    Update CompanyRepository.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/CompaniesPackage/CompanyRepository.java
+++ SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/CompaniesPackage/CompanyRepository.java
@@ -1,10 +1,17 @@
 package com.example.SagarNaukri.com.CompaniesPackage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<Company,Integer> {
     Optional<Company> findByEmail(String email);
     Optional<Company> findByEmailAndPassword(String email,String password);
+
+    @Query("select e.companyname from Company e where e.email= :email")
+    String findCompanyNameByEmail(@Param("email") String email);
+
+
 }
