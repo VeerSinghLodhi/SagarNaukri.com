991ba9b2b6ecd8fe8e5d8fbf42ad2bddafdeaf97
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-08-04 23:18:38 +0530

    Add files via upload

---
+++ SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/ApplicationPackage/ApplicationRepository.java
@@ -0,0 +1,13 @@
+package SagarNaukriMerge.SagarNaukriMerge.ApplicationPackage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Applications,Integer> {
+
+    List<Applications>findByJsid(Long jobseekerId);
+
+}
