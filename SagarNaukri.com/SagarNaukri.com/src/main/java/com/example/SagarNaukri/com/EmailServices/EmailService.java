352dcc6ab22c1d51803e3d167712f8c38da1a335
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-05 22:58:25 +0530

    Update EmailService.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/EmailServices/EmailService.java
+++ SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/EmailServices/EmailService.java
@@ -68,15 +68,6 @@ public class EmailService {
         helper.setFrom("contactjobsagar@gmail.com");
         helper.setTo(email);
         helper.setSubject("Welcome to "+companyName);
-//        String htmlBody = "Hi <span>" + "userName" + "</span><br>" +
-//                "<p>Thank you for registering with <span>" + companyName + "</span>. We're thrilled to have you on board!</p>" +
-//                "<p>To complete your registration, please use the following OTP:</p>" +
-//                "<h2 style='color: #4CAF50; text-align: center;'>" + otp + "</h2>" +
-//                "<p style='color: #555;'>This OTP is valid for the next <strong>10 minutes</strong>. If you did not request this, please ignore this email or contact our support team.</p>" +
-//                "<br>" +
-//                "<p>Need help? Reach out to us at <span>contactjobsagar@gmail.com</span>.</p>" +
-//                "<h3>Welcome aboard!</h3><br>" +
-//                "<h3>" + companyName + "</h3>";
 
         String htmlBody = "<!DOCTYPE html>" +
                 "<html lang='en'>" +
@@ -185,3 +176,4 @@ public class EmailService {
     }
 
 }
+
