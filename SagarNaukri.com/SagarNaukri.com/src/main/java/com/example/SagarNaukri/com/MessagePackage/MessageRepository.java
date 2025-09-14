a6c4414d5eb694edd4c2f659d11cb2a84149b6b0
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-14 21:55:48 +0530

    Delete SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/MessagePackage/MessageRepository.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/MessagePackage/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package SagarNaukriMerge.SagarNaukriMerge.MessagePackage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface MessageRepository extends JpaRepository<Message,Long> {
-    List<Message> findByConversationOrderByTimestampAsc(Conversation conversation);
-}
