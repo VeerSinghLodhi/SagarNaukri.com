701c09ff81ac8a0fa79a3d307af069b6f18f606e
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-09-15 22:17:42 +0530

    Delete SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/MessagePackage/ConversationRepository.java

--- SagarNaukri.com/SagarNaukri.com/src/main/java/com/example/SagarNaukri/com/MessagePackage/ConversationRepository.java
+++
@@ -1,9 +0,0 @@
-package SagarNaukriMerge.SagarNaukriMerge.MessagePackage;
-
-import SagarNaukriMerge.SagarNaukriMerge.CompaniesPackage.Company;
-import SagarNaukriMerge.SagarNaukriMerge.JobSeeker.JobSeeker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConversationRepository extends JpaRepository<Conversation,Long> {
-    Conversation findByCompanyAndJobSeeker(Company company, JobSeeker jobSeeker);
-}
